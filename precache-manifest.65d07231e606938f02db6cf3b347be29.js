f703a269c79d8e9ab84ed88d7aae62bb4cd0f3db
tzils <tzil.saar@gmail.com>
2019-10-03 15:03:28 +0200

    Updates

---
+++ precache-manifest.65d07231e606938f02db6cf3b347be29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c61558c998112450d1f1353c767d026",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "d4dcf91a663f1ffed723",
+    "url": "/cv/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7fbd16b27b875ad66076",
+    "url": "/cv/static/js/2.a3cc1d30.chunk.js"
+  },
+  {
+    "revision": "d4dcf91a663f1ffed723",
+    "url": "/cv/static/js/main.9deb6d41.chunk.js"
+  },
+  {
+    "revision": "be71a976392a8d1a5397",
+    "url": "/cv/static/js/runtime-main.128d7ca1.js"
+  }
+]);
